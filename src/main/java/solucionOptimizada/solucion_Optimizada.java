63135afeccece1881e23b61430c11281f46bd02b
Edwar-Forero <edwar.forero@correounivalle.edu.co>
2023-11-22 19:53:43 -0500

    prueba ramas

--- src/main/java/solucionOptimizada/solucion_Optimizada.java
+++ src/main/java/solucionOptimizada/solucion_Optimizada.java
@@ -1,4 +1,13 @@
 package solucionOptimizada;
 
 public class solucion_Optimizada {
+
+    solucion_Optimizada(int numeroEquipos, int tamanoMinimo, int tamanoMaximo, int[][] matrizDistancias) {
+
+
+    }
+
+    public int getNumeroEquipos() {
+        return 0;
+    }
 }
