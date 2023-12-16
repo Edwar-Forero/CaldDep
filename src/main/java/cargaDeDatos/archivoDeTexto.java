c350b1231eca568fbde62a7950d5852fc43a6cad
Edwar-Forero <edwar.forero@correounivalle.edu.co>
2023-12-15 21:07:28 -0500

    Actualizacion archivoDeTexto

--- src/main/java/cargaDeDatos/archivoDeTexto.java
+++ src/main/java/cargaDeDatos/archivoDeTexto.java
@@ -1,11 +1,10 @@
 package cargaDeDatos;
 import solucionIngenua.solucion_Ingenua;
 import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
+
 
 /**
  * Esta clase permite cargar los datos de un archivo de texto
@@ -14,86 +13,59 @@ import java.io.IOException;
  */
 
 public class archivoDeTexto {
+
     /**
      * Este método permite cargar los datos de un archivo de texto
-     * @param args mensaje que se muestra en la ventana de carga de archivos
+     * @param args argumentos de la línea de comandos
      *
      */
     public static void main(String[] args) {
-        // Crear un objeto JFileChooser
         JFileChooser fileChooser = new JFileChooser();
-
-        // Configurar el filtro para mostrar solo archivos de texto
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("Archivos de Texto", "txt", "text", "csv", "dat");
-        fileChooser.setFileFilter(filter);
-
-        // Mostrar el diálogo de selección de archivo
         int result = fileChooser.showOpenDialog(null);
 
-        // Procesar la selección del usuario
         if (result == JFileChooser.APPROVE_OPTION) {
-            // Obtener el archivo seleccionado
             File selectedFile = fileChooser.getSelectedFile();
+            leerArchivo(selectedFile);
+        }
+    }
 
-            try {
-                // Leer el contenido del archivo
-                String fileContent = readFile(selectedFile);
+    /**
+     * Este método permite leer los datos de un archivo de texto
+     * @param file archivo de texto que se va a leer
+     *
+     */
+    private static void leerArchivo(File file) {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            /**
+             * Leer la primera línea (numEquipos)
+             * @param numEquipos número de equipos
+             *
+             */
+            int numEquipos = Integer.parseInt(br.readLine());
 
-                // Parsear el contenido y crear una instancia de solucionIngenua
-                solucion_Ingenua solucion = parseFileContent(fileContent);
+            // Leer la segunda línea (min)
+            int min = Integer.parseInt(br.readLine());
 
-                // Acceder a las variables de solucionIngenua según sea necesario
-                System.out.println("Número de Equipos: " + solucion.getNumeroEquipos());
-                System.out.println("Tamaño Mínimo: " + solucion.getTamanoMinimo());
-                System.out.println("Tamaño Máximo: " + solucion.getTamanoMaximo());
-                System.out.println("Matriz de Distancias:");
-                int[][] matrizDistancias = solucion.getMatrizDistancias();
-                for (int[] matrizDistancia : matrizDistancias) {
-                    for (int i : matrizDistancia) {
-                        System.out.print(i + " ");
-                    }
-                    System.out.println();
+            // Leer la tercera línea (max)
+            int max = Integer.parseInt(br.readLine());
+
+            // Leer la matriz de distancias
+            int[][] matrizDistancias = new int[numEquipos][numEquipos];
+            for (int i = 0; i < numEquipos; i++) {
+                String[] distancias = br.readLine().split(" ");
+                for (int j = 0; j < numEquipos; j++) {
+                    matrizDistancias[i][j] = Integer.parseInt(distancias[j]);
                 }
-
-            } catch (IOException e) {
-                System.err.println("Error al leer el archivo: " + e.getMessage());
             }
-        } else {
-            System.out.println("Operación cancelada por el usuario");
+
+            // Crear una instancia de solucion_Ingenua y pasar la información
+            //solucion_Ingenua solucion = new solucion_Ingenua(numEquipos, min, max, matrizDistancias);
+
+            // Imprimir la información (puedes personalizar esto según tus necesidades)
+            //solucion.imprimirInformacion();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
-
-    private static String readFile(File file) throws IOException {
-        StringBuilder content = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                content.append(line).append("\n");
-            }
-        }
-        return content.toString();
-    }
-
-    private static solucion_Ingenua parseFileContent(String fileContent) {
-        String[] lines = fileContent.split("\n");
-
-        // Extraer valores de las primeras tres líneas
-        int numeroEquipos = Integer.parseInt(lines[0]);
-        int tamanoMinimo = Integer.parseInt(lines[1]);
-        int tamanoMaximo = Integer.parseInt(lines[2]);
-
-        // Inicializar la matriz de distancias
-        int[][] matrizDistancias = new int[numeroEquipos][numeroEquipos];
-
-        // Llenar la matriz de distancias desde las líneas restantes
-        for (int i = 0; i < numeroEquipos; i++) {
-            String[] distances = lines[i + 3].split(" ");
-            for (int j = 0; j < numeroEquipos; j++) {
-                matrizDistancias[i][j] = Integer.parseInt(distances[j]);
-            }
-        }
-
-        // Crear y devolver una instancia de la clase solucionIngenua
-        return new solucion_Ingenua(numeroEquipos, tamanoMinimo, tamanoMaximo, matrizDistancias);
     }
 }
